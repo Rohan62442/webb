2b09cb46471568a8660ac0b7f5229115ac418f2b
Rohan Pinto <rohan62442@hotmail.com>
2015-05-17 22:20:26 +0530

    Fixes #19

--- run/creating.py
+++ run/creating.py
@@ -6,14 +6,15 @@ def get_hash(name):
     import hashlib
     return hashlib.sha1(name.encode('utf-8')).hexdigest()
 
-def get_json(name, sha, url):
+def get_json(name, label, sha, url):
     import json
-    return json.dumps({"name": name.lower(),"label": name,"icon": "default.svg","comment": "","url": url,"profile": sha})
+    return json.dumps({"name": name,"label": label,"icon": "default.svg","comment": "","url": url,"profile": sha})
 
-def create_new(name, url):
-    if name.lower() in get_all_apps():
-        print(name+" is already available.")
+def create_new(label, url):
+    name=label.lower().strip().replace(" ", "-")
+    if name in get_all_apps():
+        print(label+" is already available.")
         return
-    with open("apps/"+name.lower()+".json", 'w') as f:
-        f.write(get_json(name, get_hash(name), url))
-    install(name.lower())
+    with open("apps/"+name+".json", 'w') as f:
+        f.write(get_json(name, label, get_hash(label), url))
+    install(name)
